c02ef5a4f33767c04908caed0b26feb6ff84873a
Zixuan740 <zixuan.chen3@students.mq.edu.au>
2026-09-14 17:09:48 +0800

    Set up Simulation constants and random generator

---
+++ predatorPrey/src/Simulation.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+
+public class Simulation {
+    public static final int WORLD_WIDTH = 800;
+    public static final int WORLD_HEIGHT = 600;
+
+    private static final int INITIAL_PREDATORS = 5;
+    private static final int INITIAL_PREY = 20;
+    private static final int INITIAL_GRASS = 100;
+
+    private int tick;
+    private int day;
+
+    private Random random; 
+
+    public Simulation() {
+        random = new Random();
+
+    }
+
+
+
+
+}
