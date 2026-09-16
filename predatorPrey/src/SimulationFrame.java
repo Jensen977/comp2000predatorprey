1997619a20549db249ae97fb7e965a80cbc48045
Zixuan740 <zixuan.chen3@students.mq.edu.au>
2026-09-16 15:27:29 +0800

    Complete simulation update and statistics label logic

--- predatorPrey/src/SimulationFrame.java
+++ predatorPrey/src/SimulationFrame.java
@@ -1,17 +1,21 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+
 
 public class SimulationFrame extends JFrame{
     
     private final Simulation simulation;
     private final SimulationPanel simulationPanel; 
 
+    private final JLabel statisticsLabel; 
+
     public SimulationFrame() {
         super("Predator-Prey Simulation");
 
         simulation = new Simulation(); 
         simulationPanel = new SimulationPanel(simulation); 
-
+        statisticsLabel = new JLabel(); 
 
     }
 
@@ -25,5 +29,20 @@ public class SimulationFrame extends JFrame{
 
     public void pauseSimulation() {
 
+    }
+
+    public void updateSimulation() {
+        simulation.update();
+        simulationPanel.repaint();
+        updateLabels(); 
+    }
+
+    public void updateLabels() {
+        statisticsLabel.setText(String.format(
+                "Day %d  |  Predators: %d  Prey %d  Grass: %d", 
+                simulation.getDay(),
+                simulation.getPredatorCount(), 
+                simulation.getPreyCount(),
+                simulation.getEdibleGrassCount())); 
     }
 }
