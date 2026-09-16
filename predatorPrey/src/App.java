394ab83d02f3173cbafbf3b882b199cee758cee6
Zixuan740 <zixuan.chen3@students.mq.edu.au>
2026-09-16 16:58:38 +0800

    Add basic App entry point for testing

--- predatorPrey/src/App.java
+++ predatorPrey/src/App.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        List<Creature> sim = new ArrayList<>();
-        List<Grass> grassList = new ArrayList<>();
+        SimulationFrame frame = new SimulationFrame();
+        frame.setVisible(true);
     }
 }
